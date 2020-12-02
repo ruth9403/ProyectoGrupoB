f1f82754b7fddb6bbb7623c16de12d114ed1424a
Maria Isabel Jaramillo Arredondo <mi.jaramillo@uniandes.edu.co>
2020-12-01 20:29:13 -0500

    Elimine el archivo de sesion

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask_session import Session
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
 # Añado la ruta por defecto
 @app.route("/")
 def index():
