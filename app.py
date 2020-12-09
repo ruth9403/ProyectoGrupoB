d6fbee6a9b820f9e345be75101614bbece4bf43b
Maria Isabel Jaramillo Arredondo <mi.jaramillo@uniandes.edu.co>
2020-12-08 21:57:24 -0500

    agregando metodos get y post a algunas paginas

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_session import Session
+from utils import validateUser
 
 app = Flask(__name__)
 
 # Añado la ruta por defecto
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def index():
 
-    return render_template("index.html")
+    if request.method =="GET":
+        
+        return render_template("index.html", visible = False)
+
+    else:
+        username = request.form.get("usuario")
+        password = request.form.get("pass")
+
+        print(username, password)
+
+        validUser = validateUser(username, password)
+
+        if validUser:
+            return redirect("/MisBlogs")
+        else:
+
+            mensaje = "Usuario no registrado"
+
+            if username == "":
+                mensaje = "Ingrese un nombre de usuario válido"
+            
+            elif password == "":
+                mensaje = "Ingrese una contraseña válida"
+
+            return render_template("index.html", visible = True, mensaje= mensaje)
+
+
 
 @app.route("/Recuperar")
 def Recuperar():
@@ -18,19 +45,55 @@ def MisBlogs():
     
     return render_template("MisBlogs.html")
 
-@app.route("/Recuperar2")
+@app.route("/Recuperar2", methods = ["GET", "POST"])
 def Recuperar2():
 
-    return render_template("recuperacion2.html")
+    if request.method =="GET":
+        return render_template("recuperacion2.html")
+    
+    else:
+
+        newpass = request.form.get("password")
+        confirmpass = request.form.get("password2")
+
+        # Validación de que ambas contraseñas sen las mismas
+
+        if newpass == confirmpass:
+
+            # Hacer un update a la base de datos
+
+            # Si las contraseñas coinciden redirijo al usuario a la pagina de inicio para que se loguee
+            return redirect("/")
+
+
+
+
 @app.route("/header")
 def header():
 
     return render_template("header.html")
 
-@app.route("/registro")
+@app.route("/registro", methods = ["GET", "POST"])
 def registro():
 
-    return render_template("registro.html")
+    if request.method == "GET":
+        return render_template("registro.html")
+    
+    else:
+
+        username = request.form.get("username")
+        correo = request.form.get("email")
+        password = request.form.get("password")
+        confirm = request.form.get("passCheck")
+
+        # Insertar validaciones de la profesora
+        # Verificar que la contraseña y su confirmación sean iguales
+
+        # Estos datos deben usarse para hacer un insert en la tabla de usuarios en la base de datos
+
+        # Llamar un alert de js para avisar que ha sido registrado
+
+        return redirect("/")
 
 @app.route("/resultados")
 def resultados():
---
+++ utils.py
@@ -0,0 +1,12 @@
+def validateUser(username, password):
+
+    # Aquí pondremos la validación con la base de datos
+
+    if username == "usuario" and password == "123":
+
+        return True
+    
+    else:
+        return False
+
+
